d6223e926f55e6753f06b031f2525ebf19b20604
dre258j <dre258j@gmail.com>
2025-04-11 10:09:19 -0500

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -1,5 +1,4 @@
 let urlDatabase = {};
-let clickDatabase = {};
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
@@ -7,17 +6,12 @@ export default function handler(req, res) {
     const shortId = custom || Date.now().toString(36);
 
     if (urlDatabase[shortId]) {
-      return res.status(400).json({ error: 'Alias already exists' });
+      return res.status(400).json({ error: "Custom alias already exists" });
     }
 
     urlDatabase[shortId] = url;
-    clickDatabase[shortId] = 0;
-
-    const shortenedUrl = `${req.headers.origin}/${shortId}`;
-    res.status(200).json({ shortened_url: shortenedUrl });
+    res.status(200).json({ shortened_url: `${req.headers.origin}/${shortId}` });
   } else {
-    res.status(405).end(); // Method Not Allowed
+    res.status(405).end();
   }
 }
-
-export { urlDatabase, clickDatabase };
